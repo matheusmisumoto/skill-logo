d457e1a310bf48da37866edf9656a1eda32ccf2e
Matheus Misumoto <49180175+matheusmisumoto@users.noreply.github.com>
2026-06-14 23:11:50 -0300

    Fix: Update author name in plugin header

--- skill-logo.php
+++ skill-logo.php
@@ -5,7 +5,7 @@
  * Version:           0.1.0
  * Requires at least: 6.8
  * Requires PHP:      7.4
- * Author:            The WordPress Contributors
+ * Author:            Matheus Misumoto
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       skill-logo
